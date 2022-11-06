e762c731f5a4bbf96e289c8bb7e1775d4120151c
Jozark <joshua.meyer.personal@gmail.com>
2022-11-06 15:14:08 +0100

    write test

--- programs/game/src/lib.rs
+++ programs/game/src/lib.rs
@@ -17,6 +17,15 @@ pub mod game {
         user_stats.bump = *ctx.bumps.get("user_stats").unwrap();
         Ok(())
     }
+
+    pub fn change_user_name(ctx: Context<ChangeUserName>, new_name: String) -> Result<()> {
+    if new_name.as_bytes().len() > 200 {
+        // proper error handling omitted for brevity
+        panic!();
+    }
+    ctx.accounts.user_stats.name = new_name;
+    Ok(())
+}
 }
 
 #[account]
--- tests/game.ts
+++ tests/game.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { Game } from "../target/types/game";
+import { expect } from "chai";
 
-describe("game", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+describe("game", async () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.Game as Program<Game>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Sets and changes name!", async () => {
+    const [userStatsPDA, _] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("user-stats"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    console.log(userStatsPDA, "userStatsPDA");
+
+    await program.methods
+      .createUserStats("jozark")
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc();
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).name).to.equal(
+      "jozark"
+    );
+
+    await program.methods
+      .changeUserName("jo_zark")
+      .accounts({
+        user: provider.wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc();
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).name).to.equal(
+      "jo_zark"
+    );
   });
 });
