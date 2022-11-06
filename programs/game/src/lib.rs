df2fa851a91d3cd0d24e3be6bd4d586fa36e21b8
Jozark <joshua.meyer.personal@gmail.com>
2022-11-06 14:19:06 +0100

    create initialize function for pda

--- programs/game/src/lib.rs
+++ programs/game/src/lib.rs
@@ -5,11 +5,38 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod game {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    // handler function
+    pub fn create_user_stats(ctx: Context<CreateUserStats>, name: String) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+        user_stats.level = 0;
+        if name.as_bytes().len() > 200 {
+            // proper error handling omitted for brevity
+            panic!();
+        }
+        user_stats.name = name;
+        user_stats.bump = *ctx.bumps.get("user_stats").unwrap();
         Ok(())
     }
 }
 
+#[account]
+pub struct UserStats {
+    level: u16,
+    name: String,
+    bump: u8,
+}
+
+// validation struct
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateUserStats<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    // space: 8 discriminator + 2 level + 4 name length + 200 name + 1 bump
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 2 + 4 + 200 + 1, seeds = [b"user-stats", user.key().as_ref()], bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+    pub system_program: Program<'info, System>,
+}
